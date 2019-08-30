6b4962ec0420b13f9a701343ef88b2ee6302ff54
KuanHsien <coson002fy@gmail.com>
2019-08-30 12:08:49 +0800

    Initial commit

---
+++ app/src/main/java/com/kuanhsien/app/sample/direct_pay_with_tappay_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kuanhsien.app.sample.direct_pay_with_tappay_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
